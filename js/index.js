76c75ef661df808ef5037e9e1c819c266e257395
yoshfoydalanuvchi <yoshfoydalanuvchi@gmail.com>
2024-06-12 19:46:55 +0500

    none

---
+++ js/index.js
@@ -0,0 +1,40 @@
+//1
+
+/*
+function sonlarYigindisi(son1, son2) {
+  return son1 + son2;
+}
+let sy1 = +prompt("1 - son");
+let sy2 = +prompt("2 - son");
+
+alert(sonlarYigindisi(sy1, sy2));
+*/
+
+//2
+/*
+function sonlarKopaytmasi(son1, son2) {
+  return son1 * son2;
+}
+let sk1 = +prompt("1 - son");
+let sk2 = +prompt("2 - son");
+alert(sonlarKopaytmasi(sk1, sk2));
+*/
+
+//3
+/*
+function sonniKvadrati(son) {
+  return son ** 2;
+}
+let skv = +prompt("son kiriting");
+alert(sonniKvadratni(skv));
+*/
+
+//4
+/*
+function sonniKubi(son) {
+  return son ** 3;
+}
+let sku = +prompt("son kiriting");
+
+alert(sonniKubi(sku));
+*/
